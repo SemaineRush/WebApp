6ebd25682feb0d3cd74af99059a787955bc3bf61
slay <decobert.a78@gmail.com>
2020-01-21 16:39:23 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './Components/Header';
 import Vote from './Components/Vote';
-import './App.css';
 import Sign from './Components/Sign'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div>
+    <>
       <Header/>
       <Router>
         <div className="App">
@@ -16,7 +15,7 @@ function App() {
           <Route exact path="/vote" component={ Vote } />
         </div>
       </Router>
-    </div>
+    </>
 
   );
 }
