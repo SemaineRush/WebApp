218ee32aa025fd50326a0de805d304342ec68731
Eliott <eliott.jarmoune@outlook.com>
2020-01-21 16:51:21 +0100

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Components/Header';
 import Vote from './Components/Vote';
-import './App.css';
 import Sign from './Components/Sign'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
