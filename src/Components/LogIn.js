0cb2e8616186b6c20499a49ecce00bdec4154ec0
BEY BOUMEZRAG MOHAMED <linkthebrave86@gmail.com>
2020-01-21 10:28:09 +0100

    register and login

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import '../Assets/register.css'
+import folder from '../Assets/media/folder.svg'
 
 export default class LogIn extends Component {
     render() {
         return (
-            <div>
-                <span>Login</span>
+            <div className="form_container">
+                <div className="img_frame">
+                    <img src={folder} />
+                </div>
+                <form>
+                    <div className="form_line"><h3>ADRESSE EMAIL</h3><input placeholder="" className="form_input" /></div>
+                    <div className="form_line"><h3>MOT DE PASSE</h3><input placeholder="" className="form_input" type="password" /></div>
+                    <button className="form_button" type="submit">Connexion</button>
+                </form>
             </div>
         )
     }
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import '../Assets/register.css'
+import folder from '../Assets/media/folder.svg'
 
 export default class Register extends Component {
     render() {
         return (
-            <div>
-                <span>Register</span>
+            <div className="form_container">
+                <div className="img_frame">
+                    <img src={folder} />
+                </div>
+                <form>
+                    <div className="form_line"><h3>NOM</h3><input placeholder="Votre nom" className="form_input" /></div>
+                    <div className="form_line"><h3>PRENOM</h3><input placeholder="Votre prénom" className="form_input" /></div>
+                    <div className="form_line"><h3>ADRESSE EMAIL</h3><input placeholder="" className="form_input" /></div>
+                    <div className="form_line"><h3>MOT DE PASSE</h3><input placeholder="" className="form_input" type="password" /></div>
+                    <div className="form_line"><h3>CONFIRMATION MOT DE PASSE</h3><input placeholder="" className="form_input" type="password" /></div>
+                    <button className="form_button" type="submit">SEND</button>
+                </form>
             </div>
         )
     }
