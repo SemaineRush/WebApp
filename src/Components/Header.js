c465f9800eaf8f73af30211220a040bb4963c23a
ErwanPichon <erwan.pichon@supinternet.fr>
2020-01-22 12:20:34 +0100

    header fixed on all pages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
-import '../Assets/header.css'
+import React, { Component } from 'react';
+import '../Assets/header.css';
+import Logo from '../Assets/media/supvote_logo.png';
 
 export default class Header extends Component {
-    render() {
-        return (
-            <header>
-              LOGO
-            </header>
-        )
-    }
+	render() {
+		return (
+			<header>
+				<img src={Logo} alt="logo" />
+			</header>
+		);
+	}
 }
