77effb8e611d7dfd1695bf8abc6e35519fe8418d
Jp Hayek <jphayek1@gmail.com>
2020-01-23 16:41:09 +0100

    GuiralWin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home'
 import Vote from './Components/Vote';
 import Sign from './Components/Sign'
 import GuiralVote from './Components/GuiralVote'
+import GuiralWin from './Components/GuiralWin'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/vote" component={ Vote } />
           <Route exact path="/home" component={ Home } />
           <Route exact path="/home/guiral" component={ GuiralVote } />
+          <Route exact path="/home/guiralwin" component={ GuiralWin } />
         </div>
       </Router>
     </>
---
+++ src/Components/GuiralWin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../Assets/guiralwin.css'
+
+
+export default class GuiralWin extends Component {
+    render() {
+        return (
+            <div className='guiral_win_home'>
+                <div className='guiral-background'>
+                    
+                </div>
+            </div>
+        )
+      
+    }
+
+}
