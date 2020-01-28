d712709aba4bed5d1efb39a7a6de2acda9978dab
ErwanPichon <erwan.pichon@supinternet.fr>
2020-01-28 20:24:29 +0100

    change link position / homePage

--- src/Components/HomeCandidates.js
+++ src/Components/HomeCandidates.js
@@ -1,38 +1,59 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import api from './Api';
 import '../Assets/home.css';
 
 export default class HomeCandidates extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        candidates: []
-        };
-    }
-    componentDidMount(){
-        api.getElections(this.props.CurrentElectionId).then((data) => {
-            console.log(data)
-            this.setState({
-                candidates: data['hydra:member'][0].candidateElection
-            })
-        })
-    }
-    render() {
-        return (
-            <div className="homecandidates">
-            {this.state.candidates.map(candidate => {
-            return <div className="candidat">
-						<div className="candidat_img" style={{backgroundColor: candidate.informations.color}}>
-							<img alt="d" src={candidate.informations.image_url} />
-						</div>
-						<div className="candidat_info">
-                            <Link to={`/home/${candidate.informations.lastname}`}><h3>{candidate.informations.firstname} {candidate.informations.lastname}</h3></Link>
-							<p>{candidate.informations.slogan}</p>
-						</div>
-					</div>
-            })}
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			candidates: []
+		};
+	}
+	componentDidMount() {
+		api.getElections(this.props.CurrentElectionId).then((data) => {
+			console.log(data);
+			this.setState({
+				candidates: data['hydra:member'][0].candidateElection
+			});
+		});
+	}
+	// render() {
+	//     return (
+	//         <div className="homecandidates">
+	//         {this.state.candidates.map(candidate => {
+	//         return <div className="candidat">
+	// 					<div className="candidat_img" style={{backgroundColor: candidate.informations.color}}>
+	// 						<img alt="d" src={candidate.informations.image_url} />
+	// 					</div>
+	// 					<div className="candidat_info">
+	//                         <Link to={`/home/${candidate.informations.lastname}`}><h3>{candidate.informations.firstname} {candidate.informations.lastname}</h3></Link>
+	// 						<p>{candidate.informations.slogan}</p>
+	// 					</div>
+	// 				</div>
+	//         })}
+	//         </div>
+	//     )
+	// }
+	render() {
+		return (
+			<div className="homecandidates">
+				{this.state.candidates.map((candidate) => {
+					return (
+						<Link className="candidat" to={`/home/${candidate.informations.lastname}`}>
+							<div className="candidat_img" style={{ backgroundColor: candidate.informations.color }}>
+								<img alt="d" src={candidate.informations.image_url} />
+							</div>
+							<div className="candidat_info">
+								<h3>
+									{candidate.informations.firstname} {candidate.informations.lastname}
+								</h3>
+								<p>{candidate.informations.slogan}</p>
+							</div>
+						</Link>
+					);
+				})}
+			</div>
+		);
+	}
 }
