bea4399eb2e75f0cae511abbc1434a46d2fc32a4
BEY BOUMEZRAG MOHAMED <linkthebrave86@gmail.com>
2020-01-29 08:28:31 +0100

    winner result

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,10 @@ import HomeCandidates from './HomeCandidates';
 import api from './Api';
 import '../Assets/home.css';
 import Header from './Header';
+import Arevichan from './ArevishanWin'
+import Guiral from './GuiralWin'
+import Maeliss from './maelissResult'
+
 
 export default class Home extends Component {
 	constructor(props) {
@@ -11,40 +15,62 @@ export default class Home extends Component {
 		this.state = {
 			email: '',
 			password: '',
-			currentElectionId: ''
+			currentElectionId: '',
+			electionIsOver: false,
+			electionWinner: ''
 		};
 	}
 
 	componentDidMount() {
 		api.getLastElection().then((json) => {
 			this.setState({
-				currentElectionId: json.response.last_election.id
+				currentElectionId: json.response.last_election.id,
+				electionIsOver: json.response.last_election.finished,
+				electionWinner:  json.response.last_election.winner
+				
 			});
 		});
-		// setTimeout(() => {
-		// 	console.log(this.state.currentElectionId);
-		// }, 300);
+		
 	}
 	render() {
-		return (
-			<div>
-				<Header />
-				<div className="home">
-					<div className="top_left_yellow" />
-					<div className="bottom_left_pink" />
-					<div className="bottom_right_purple" />
-					<div className="bottom_right_blue" />
-
-					<HomeCandidates CurrentElectionId={this.state.currentElectionId} />
-					<div className="yellow_top">
-						<h2>Election SUP'Internet</h2>
-						<h4>BDE 2020</h4>
+		if (this.state.electionIsOver === true) {
+			if (this.state.electionWinner === 3) {
+				return (
+					<Maeliss />
+					
+				)
+			}
+			if (this.state.electionWinner === 90) {
+				return (
+					<Arevichan />
+				)
+			}
+			if (this.state.electionWinner === 92) {
+				return (
+					<Guiral />
+				)
+			}
+		} else {
+			return (
+				<div>
+					<Header />
+					<div className="home">
+						<div className="top_left_yellow" />
+						<div className="bottom_left_pink" />
+						<div className="bottom_right_purple" />
+						<div className="bottom_right_blue" />
+						<HomeCandidates CurrentElectionId={this.state.currentElectionId} />
+						<div className="yellow_top">
+							<h2>Election SUP'Internet</h2>
+							<h4>BDE 2020</h4>
+						</div>
+						<Link className="btn" to="/vote">
+							Voter
+						</Link>
 					</div>
-					<Link className="btn" to="/vote">
-						Voter
-					</Link>
 				</div>
-			</div>
-		);
+			);
+		}
+		
 	}
 }
